f2cbbc73a3ff2bb0ecd4ff22681fb4f811610e9e
James McCray <chrisjamesjackmccray@gmail.com>
2016-03-24 15:50:35 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -1,19 +1,7 @@
 function textCount() {
   let textcount = document.querySelector('body').innerHTML.split(' ').length;
-  console.log(textcount)
+  let left = textcount / 250;
+  document.querySelector('#time-to-read').innerHTML = "read time: " + left + " minutes";
 }
 
-textCount()
-
-
-
-function left() {
-  let left = console.log(830 / 250);
-}
-left();
-
-
-function addTextNode() {
-  let newtext = document.createTextNode("read time: 3 minutes");
-  let h3 = document.querySelector('#time-to-read').innerHTML.appendChild(newtext);
-}
+textCount();
